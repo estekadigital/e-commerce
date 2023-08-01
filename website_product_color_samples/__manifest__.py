35202aa1e2e227ffc01ea93c641d6d15dda44f9d
Esteka Digital <52083572+estekadigital@users.noreply.github.com>
2023-08-01 11:00:34 +0200

    Add files via upload

---
+++ website_product_color_samples/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website product color samples",
+    'summary': """Display product color options in grid view on website""",
+    'description': """Display product color options in grid view on website""",
+    'author': 'Odoo Masters',
+    'category': 'eCommerce',
+    'version': '14.0',
+    'depends': [
+        'sale',
+        'website_sale',
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/templates.xml',
+        'views/variant_templates.xml',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
